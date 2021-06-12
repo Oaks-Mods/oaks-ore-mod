171cc380d04f9618a6186fb513730d8e14021650
OakBricks <oakbricks@gmail.com>
2021-06-12 17:16:56 -0500

    custom inventory tab

--- src/main/java/org/oakbricks/oakores/OakOres.java
+++ src/main/java/org/oakbricks/oakores/OakOres.java
@@ -1,13 +1,34 @@
 package org.oakbricks.oakores;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import org.oakbricks.oakores.registry.BlockClass;
+import org.oakbricks.oakores.registry.ItemClass;
 
+import static org.oakbricks.oakores.registry.ItemClass.registerBlockItems;
 import static org.oakbricks.oakores.registry.ItemClass.registerItems;
 import static org.oakbricks.oakores.registry.BlockClass.registerBlocks;
 
 public class OakOres implements ModInitializer {
 
 	public static final String MOD_ID = "oakores";
+
+
+	public static final ItemGroup MAIN_GROUP = FabricItemGroupBuilder.create(
+			new Identifier("tutorial", "other"))
+			.icon(() -> new ItemStack(BlockClass.PURPI_BLOCK))
+			.appendItems(stacks -> {
+				stacks.add(new ItemStack(BlockClass.PURPI_BLOCK));
+				stacks.add(new ItemStack(BlockClass.PURPI_ORE));
+				stacks.add(new ItemStack(ItemClass.PUPRI));
+			})
+			.build();
+
 
 	@Override
 	public void onInitialize() {
@@ -17,6 +38,7 @@ public class OakOres implements ModInitializer {
 
 		registerItems();
 		registerBlocks();
-		System.out.println("Hello Fabric world!");
+		registerBlockItems();
+		System.out.println("Oak's Ore Mod Initalized!");
 	}
 }
--- src/main/java/org/oakbricks/oakores/registry/ItemClass.java
+++ src/main/java/org/oakbricks/oakores/registry/ItemClass.java
@@ -1,21 +1,33 @@
 package org.oakbricks.oakores.registry;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.oakbricks.oakores.OakOres;
 
 import static org.oakbricks.oakores.OakOres.MOD_ID;
 
 public class ItemClass {
 
-    public static final Item PUPRI = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item PUPRI = new Item(new FabricItemSettings().group(OakOres.MAIN_GROUP));
 
     public static void registerItems() {
 
         Registry.register(Registry.ITEM, new Identifier(MOD_ID, "purpi"), PUPRI);
 
     }
+
+
+    public static void registerBlockItems() {
+
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "purpi_block"), new BlockItem(BlockClass.PURPI_BLOCK, new FabricItemSettings().group(OakOres.MAIN_GROUP)));
+
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "purpi_ore"), new BlockItem(BlockClass.PURPI_ORE, new FabricItemSettings().group(OakOres.MAIN_GROUP)));
+
+    }
+
 
 }
