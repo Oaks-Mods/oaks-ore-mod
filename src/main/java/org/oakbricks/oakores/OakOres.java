e2d82c94403d127bafcb2feef83b56ac39ef15fa
OakBricks <oakbricks@gmail.com>
2021-06-13 07:45:11 -0500

    improve on the config situation a bit

--- src/main/java/org/oakbricks/oakores/OakOres.java
+++ src/main/java/org/oakbricks/oakores/OakOres.java
@@ -1,12 +1,18 @@
 package org.oakbricks.oakores;
 
+import me.lortseam.completeconfig.gui.ConfigScreenBuilder;
+import me.lortseam.completeconfig.gui.cloth.ClothConfigScreenBuilder;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.oakbricks.oakores.config.ModConfig;
 import org.oakbricks.oakores.registry.BlockClass;
 import org.oakbricks.oakores.registry.ItemClass;
 
@@ -17,14 +23,9 @@ public class OakOres implements ModInitializer {
 
 	public static final String MOD_ID = "oakores";
 	public static final Logger LOGGER = LogManager.getLogger();
-	private static Settings settings;
+    public static final ModConfig CONFIG = AutoConfig.register(ModConfig.class, GsonConfigSerializer::new).getConfig();
 
-	public static Settings getSettings() {
-		return settings;
-	}
-
-
-	public static final ItemGroup MAIN_GROUP = FabricItemGroupBuilder.create(
+    public static final ItemGroup MAIN_GROUP = FabricItemGroupBuilder.create(
 			new Identifier("tutorial", "other"))
 			.icon(() -> new ItemStack(BlockClass.PURPI_BLOCK))
 			.appendItems(stacks -> {
@@ -38,15 +39,13 @@ public class OakOres implements ModInitializer {
 			})
 			.build();
 
+//	AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		settings = new Settings();
-		settings.load();
 
 
 		registerItems();
---
+++ src/main/java/org/oakbricks/oakores/config/ModMenuConfig.java
@@ -0,0 +1,13 @@
+package org.oakbricks.oakores.config;
+
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
+import io.github.prospector.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+
+public class ModMenuConfig implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+    }
+}
