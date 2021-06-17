ba54b3986fbd79278ae087758fec93aafac80b80
OakBricks <oakbricks@gmail.com>
2021-06-17 16:08:37 -0500

    fix compile errors (i hope)

--- src/main/java/org/oakbricks/oakores/OakOres.java
+++ src/main/java/org/oakbricks/oakores/OakOres.java
@@ -4,34 +4,27 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.gen.GenerationStep;
-import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.decorator.Decorator;
 import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
-import net.minecraft.world.gen.heightprovider.UniformHeightProvider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.oakbricks.oakores.registry.BlockClass;
 import org.oakbricks.oakores.registry.ItemClass;
 import org.oakbricks.oakores.tools.RegisterTools;
 
-import static net.minecraft.server.command.CommandManager.literal;
 import static org.oakbricks.oakores.registry.BlockClass.registerBlocks;
 import static org.oakbricks.oakores.registry.ItemClass.*;
 import static org.oakbricks.oakores.tools.RegisterTools.registerTools;
-//import static org.oakbricks.oakores.util.OreClass.*;
 
 //don't remove these you'll break most of the mod
 
@@ -62,14 +55,14 @@ public class OakOres implements ModInitializer {
 
 	private static ConfiguredFeature<?, ?> PURPI_ORE_OVERWORLD = Feature.ORE
 			.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockClass.PURPI_ORE.getDefaultState(), 6)) /* TODO: Make this configurable */
-			.decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(UniformHeightProvider.create(YOffset.fixed(Integer.parseInt("5")), YOffset.fixed(Integer.parseInt("28")))))) /* TODO: Make this configurable */
+			.decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(0, 0, 32)))
 			.spreadHorizontally()
 			.repeat(Integer.parseInt("10")); //TODO: Make this configurable
 
 	//LEAD ORE WORLD GEN
 	private static ConfiguredFeature<?, ?> LEAD_ORE_OVERWORLD = Feature.ORE
-			.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockClass.LEAD_ORE.getDefaultState(), 6)) /* TODO: Make this configurable */
-			.decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(UniformHeightProvider.create(YOffset.fixed(Integer.parseInt("0")), YOffset.fixed(Integer.parseInt("48")))))) /* TODO: Make this configurable */
+			.configure(new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD, BlockClass.LEAD_ORE.getDefaultState(), 8)) /* TODO: Make this configurable */
+			.decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(0, 0, 48))) /* TODO: Make this configurable */
 			.spreadHorizontally()
 			.repeat(Integer.parseInt("25")); //TODO: Make this configurable
 
@@ -81,11 +74,11 @@ public class OakOres implements ModInitializer {
 		// Proceed with mild caution.
 
 		//Registers Purpi Ore world gen
-		RegistryKey<ConfiguredFeature<?, ?>> purpiOreOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY, new Identifier(MOD_ID, "purpi_ore_overworld"));
+		RegistryKey<ConfiguredFeature<?, ?>> purpiOreOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN, new Identifier(MOD_ID, "purpi_ore_overworld"));
 		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, purpiOreOverworld.getValue(), PURPI_ORE_OVERWORLD);
 		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, purpiOreOverworld);
 
-		RegistryKey<ConfiguredFeature<?, ?>> leadOreOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY, new Identifier(MOD_ID, "lead_ore_overworld"));
+		RegistryKey<ConfiguredFeature<?, ?>> leadOreOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN, new Identifier(MOD_ID, "lead_ore_overworld"));
 		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, leadOreOverworld.getValue(), LEAD_ORE_OVERWORLD);
 		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, leadOreOverworld);
 
--- src/main/java/org/oakbricks/oakores/OakOresClient.java
+++ src/main/java/org/oakbricks/oakores/OakOresClient.java
@@ -1,6 +1,5 @@
 package org.oakbricks.oakores;
 
-import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
@@ -9,12 +8,10 @@ import net.minecraft.network.MessageType;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Util;
-import org.oakbricks.oakores.config.ModConfig;
 import net.minecraft.entity.player.PlayerEntity;
 import org.oakbricks.oakores.registry.BlockClass;
 
 import static net.minecraft.server.command.CommandManager.literal;
-import static org.oakbricks.oakores.OakOres.CONFIG;
 
 public class OakOresClient implements ClientModInitializer {
     @Override
