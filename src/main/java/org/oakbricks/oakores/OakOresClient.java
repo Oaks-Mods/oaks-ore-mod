6aa7fbe0f45097f787b34a4dd4b1affa4c9cde7a
OakBricks <oakbricks@gmail.com>
2021-06-16 22:49:14 -0500

    colored lead ore layer, lead ore block item

--- src/main/java/org/oakbricks/oakores/OakOresClient.java
+++ src/main/java/org/oakbricks/oakores/OakOresClient.java
@@ -2,6 +2,7 @@ package org.oakbricks.oakores;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.MessageType;
@@ -10,18 +11,16 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Util;
 import org.oakbricks.oakores.config.ModConfig;
 import net.minecraft.entity.player.PlayerEntity;
+import org.oakbricks.oakores.registry.BlockClass;
 
 import static net.minecraft.server.command.CommandManager.literal;
 import static org.oakbricks.oakores.OakOres.CONFIG;
 
 public class OakOresClient implements ClientModInitializer {
-
-    MinecraftClient mc = MinecraftClient.getInstance();
-
-
     @Override
     public void onInitializeClient() {
 
+        ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> 0x444f53, BlockClass.LEAD_BLOCK);
 
     }
 
