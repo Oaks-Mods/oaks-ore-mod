d409c1fbcd1f3558c42a93f7925d10933512b460
OakBricks <oakbricks@gmail.com>
2021-06-21 21:18:00 -0500

    clean up imports in PlayerEntityMixin

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # Oak's Ore Mod (Now updated for 1.17)
 ##### insert epic subtitle here!
 
-## WARNING: PLEASE DONT USE THE LATEST COMMITS USE [This](https://github.com/OakBricks/oaks-ore-mod/actions/runs/955705609), ITS THE LAST STABLE USABLE COMMIT!
-![GitHub](https://img.shields.io/github/license/OakBricks/oaks-ore-mod?color=green&style=for-the-badge) ![For Fabric](https://img.shields.io/badge/Made%20For-Fabric-blue?style=for-the-badge&logo=java)
-
 [Forge port](https://github.com/OakBricks/oak-ore-mod-forge)
 
 This is a Fabric Mod for 1.17 (no snapshots! must wait for full relase for fabric api to stable!!!)
--- src/main/java/org/oakbricks/oakores/mixin/PlayerEntityMixin.java
+++ src/main/java/org/oakbricks/oakores/mixin/PlayerEntityMixin.java
@@ -1,31 +1,18 @@
 package org.oakbricks.oakores.mixin;
 
 // this was and is very painful to code for
-import net.minecraft.entity.Entity;
+
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.PlayerManager;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.world.Difficulty;
-import net.minecraft.world.GameMode;
-import net.minecraft.world.GameRules;
-import net.minecraft.world.World;
 import org.oakbricks.oakores.registry.ItemClass;
-import org.oakbricks.oakores.util.TickUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import static java.lang.Thread.sleep;
 
 @Mixin(LivingEntity.class)
 public abstract class PlayerEntityMixin implements EntityAccessor {
@@ -46,6 +33,9 @@ public abstract class PlayerEntityMixin implements EntityAccessor {
 
     @Inject(at = @At("HEAD"), method = "tick")
     public void leadOnTick(CallbackInfo ci) {
+
+        leadPoisonCounterIncrement();
+
         if (this.getMainHandStack().isOf(ItemClass.LEAD_ROCK) || this.getOffHandStack().isOf(ItemClass.LEAD_ROCK) && this.ticks == this.maxLeadTimeAllowed() && this.getWorld().getDifficulty() != Difficulty.PEACEFUL) {
             if (this.ticks > this.maxLeadTimeAllowed()) {
                 this.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 50));
