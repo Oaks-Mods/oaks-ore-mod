ce73d29c0e0b21c6e367a7773801b09bb6c28d86
OakBricks <oakbricks@gmail.com>
2021-08-08 00:05:25 -0500

    clean up comments in OakOres.java

--- src/main/java/org/oakbricks/oakores/OakOres.java
+++ src/main/java/org/oakbricks/oakores/OakOres.java
@@ -35,58 +35,6 @@ public class OakOres implements ModInitializer {
 			new Identifier(MOD_ID, "oakores"),
 			() -> new ItemStack(ModBlocks.PURPI_BLOCK));
 
-//	public static final ItemGroup OAKORES_ITEM_GROUP = FabricItemGroupBuilder.create(
-//			new Identifier(MOD_ID, "items"))
-//			.icon(() -> new ItemStack(ModItems.PURPI))
-//			.appendItems(stacks -> {
-//				//items here
-//				stacks.add(new ItemStack(ModItems.LEAD_ROCK));
-//				stacks.add(new ItemStack(ModItems.PURPI));
-//				stacks.add(new ItemStack(ModItems.PURPI_BOOTS));
-//				stacks.add(new ItemStack(ModItems.PURPI_LEGGINGS));
-//				stacks.add(new ItemStack(ModItems.PURPI_CHESTPLATE));
-//				stacks.add(new ItemStack(ModItems.PURPI_HELMET));
-//			})
-//			.build();
-//
-//	public static final ItemGroup OAKORES_BLOCK_GROUP = FabricItemGroupBuilder.create(
-//			new Identifier(MOD_ID, "blocks"))
-//			.icon(() -> new ItemStack(ModBlocks.PURPI_BLOCK))
-//			.appendItems(stacks -> {
-//				//blocks here
-//				stacks.add(new ItemStack(ModBlocks.LEAD_ORE));
-//				stacks.add(new ItemStack(ModBlocks.DEEPSLATE_LEAD_ORE));
-//				stacks.add(new ItemStack(ModBlocks.LEAD_BLOCK));
-//				stacks.add(new ItemStack(ModBlocks.PURPI_BLOCK));
-//				stacks.add(new ItemStack(ModBlocks.PURPI_ORE));
-//			})
-//			.build();
-//
-//	public static final ItemGroup OAKORES_TOOL_GROUP = FabricItemGroupBuilder.create(
-//			new Identifier(MOD_ID, "tools"))
-//			.icon(() -> new ItemStack(RegisterTools.PURPI_PICKAXE))
-//			.appendItems(stacks -> {
-//				//tools here
-//				stacks.add(new ItemStack(RegisterTools.PURPI_AXE));
-//				stacks.add(new ItemStack(RegisterTools.PURPI_PICKAXE));
-//				stacks.add(new ItemStack(RegisterTools.PURPI_HOE));
-//				stacks.add(new ItemStack(RegisterTools.PURPI_SWORD));
-//				stacks.add(new ItemStack(RegisterTools.PURPI_SPADE));
-//			})
-//			.build();
-//
-//	public static final ItemGroup OAKORES_ARMOR_GROUP = FabricItemGroupBuilder.create(
-//			new Identifier(MOD_ID, "armor"))
-//			.icon(() -> new ItemStack(PURPI_HELMET))
-//			.appendItems(stacks -> {
-//				stacks.add(new ItemStack(ModItems.PURPI_BOOTS));
-//				stacks.add(new ItemStack(ModItems.PURPI_LEGGINGS));
-//				stacks.add(new ItemStack(ModItems.PURPI_CHESTPLATE));
-//				stacks.add(new ItemStack(ModItems.PURPI_HELMET));
-//			})
-//			.build();
-
-
 	@Override
 	public void onInitialize() {
         //FOR CONTRIBUTORS: please make at least 90% of modified classes/voids with names that are easy to understand!
